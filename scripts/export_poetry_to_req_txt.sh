096ec80ca42ec6bcf5b444f1b59a138577be4ed4
Brent Stone <brent.jk.stone@gmail.com>
2024-02-25 19:27:56 -0500

    Tweak typos/docs
    Minor edits to fix typos and add clarity in pros-cons docs

--- scripts/export_poetry_to_req_txt.sh
+++ scripts/export_poetry_to_req_txt.sh
@@ -4,4 +4,4 @@ poetry export --format requirements.txt --output requirements/requirements.txt -
 poetry export --format requirements.txt --output requirements/requirements-dev.txt --without-hashes --with dev
 poetry export --format requirements.txt --output requirements/requirements-pytest.txt --without-hashes --with pytest
 poetry export --format requirements.txt --output requirements/requirements-mypy.txt --without-hashes --with mypy
-poetry export --format requirements.txt --output requirements/requirements-mkdocs.txt --without-hashes --with mkdocs
+poetry export --format requirements.txt --output requirements/requirements-mkdocs.txt --without-hashes --only mkdocs
